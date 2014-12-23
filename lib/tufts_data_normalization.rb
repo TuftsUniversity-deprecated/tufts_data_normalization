10060510f974cd6f70a38fa1a9807b559b9c84db
Mike Korcynski <mkorcy@gmail.com>
2014-12-23 10:48:46 -0500

    fixes path to railtie

--- lib/tufts_data_normalization.rb
+++ lib/tufts_data_normalization.rb
@@ -1,6 +1,6 @@
 require "tufts_data_normalization/version"
 
 module TuftsDataNormalization
-  require 'tufts_data_normalization/railtie' if defined?(Rails)
+  require 'railtie' if defined?(Rails)
   
 end

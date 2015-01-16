854f1206536e6ce447ef4efff7131ae2190b2940
Mike Korcynski <mkorcy@gmail.com>
2015-01-16 14:33:06 -0500

    exposes more tasks via railtie

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -7,7 +7,10 @@ module TuftsDataNormalization
     rake_tasks do
       load "tasks/aah_data_normalization.rake"
       load "tasks/elections_data_normalization.rake"
+      load "tasks/election_images_data_normalization.rake"
       load "tasks/perseus_data_normalization.rake"
+      load "tasks/generic_dca_wo_created_by_normalization.rake"
+      load "tasks/generic_dca_dl_normalization.rake"
     end
   end
 end
